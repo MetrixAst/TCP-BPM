7106d570dbb1865ee56bbcdd49d64392cc8d2c64
Anel <anelalibek69@gmail.com>
2026-04-15 14:54:28 +0500

    TASK-1.1: Заменила статусы задач в enums.py

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_celery_results',
+    'django_celery_beat',
     'mptt',
     'django_mptt_admin',
     'betterforms',
