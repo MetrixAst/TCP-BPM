f49684d3ce3a9918fb94e65bef5db725ebf8debf
Darya <briliantik795@gmail.com>
2026-04-29 16:10:27 +0500

    TASK-2.9: model WorkCalendar

--- backend/apps/hr/admin.py
+++ backend/apps/hr/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Company, Position
+from .models import Company, Position, WorkCalendar
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
@@ -11,3 +11,15 @@ class PositionAdmin(admin.ModelAdmin):
     list_display = ('title', 'department')
     list_filter = ('department',)
     search_fields = ('title',)
+
+@admin.register(WorkCalendar)
+class WorkCalendarAdmin(admin.ModelAdmin):
+    list_display = ('date', 'day_type', 'company', 'year')
+    list_filter = ('company', 'year', 'day_type')
+    
+    search_fields = ('date',)
+    
+    date_hierarchy = 'date'
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('company')
--- backend/apps/hr/models.py
+++ backend/apps/hr/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from account.models import UserAccount
 
-from .enums import CalendarItemType
+from .enums import CalendarItemType, DayTypeEnum
 
 
 class CalendarItem(models.Model):
@@ -74,3 +74,41 @@ class Position(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.department.name})"
+
+class WorkCalendar(models.Model):
+    company = models.ForeignKey(
+        'hr.Company', 
+        on_delete=models.CASCADE, 
+        related_name='work_calendars',
+        verbose_name="Компания",
+        null=True,   
+        blank=True   
+    )
+    year = models.IntegerField("Год")
+    date = models.DateField("Дата")
+    day_type = models.CharField(
+        "Тип дня", 
+        max_length=20, 
+        choices=DayTypeEnum.choices, 
+        default=DayTypeEnum.WORKING
+    )
+
+    class Meta:
+        verbose_name = "Производственный календарь"
+        verbose_name_plural = "Производственный календарь"
+        unique_together = ('company', 'date')
+        ordering = ['date']
+
+    def __str__(self):
+        company_name = self.company.name if self.company else "Общий (РК)"
+        return f"{self.date} ({company_name})"
+
+    @staticmethod
+    def is_working_day(date_to_check, company):
+        day_record = WorkCalendar.objects.filter(company=company, date=date_to_check).first()
+        if not day_record:
+            day_record = WorkCalendar.objects.filter(company__isnull=True, date=date_to_check).first()
+        if day_record:
+            return day_record.day_type == DayTypeEnum.WORKING
+        
+        return date_to_check.weekday() < 5
