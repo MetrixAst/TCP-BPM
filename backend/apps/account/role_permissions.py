94cb979aa5b959dbf8e08c8d4fb02f1376c734b6
Darya <briliantik795@gmail.com>
2026-08-12 14:38:12 +0500

    fix: resolve merge conflict in role_permissions.py

--- backend/apps/account/role_permissions.py
+++ backend/apps/account/role_permissions.py
@@ -375,11 +375,8 @@ class MenuItem:
                 # ]),
                 MenuItem('ecopark', 'ecopark:home', 'water', 'Эксплуатация', permission=PermissionEnums.ECOPARK),
                 MenuItem('tickets', 'tickets:kanban', 'notebook-1', 'Заявки от арендаторов', indicator_alias='ticket', permission=PermissionEnums.SERVICE_REQUESTS),
-<<<<<<< HEAD
                 MenuItem('ticket_approvals', 'tickets:approvals', 'inbox-check', 'Согласования', permission=PermissionEnums.SERVICE_REQUESTS),
-=======
                 MenuItem('ticket_workflow', 'tickets:workflow_settings', 'gear', 'Workflow заявок', permission=PermissionEnums.MANAGE_PERMISSIONS),
->>>>>>> origin/feature/FE-11-workflow-settings
                 MenuItem('reports', 'reports:home', 'eye', 'Показатели', permission=PermissionEnums.REPORTS),
             ],
 
